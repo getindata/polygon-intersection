74fb003caae6fb29b51411bb036433c167586d2b
Kamil Gorlo <kgorlo@gmail.com>
2016-03-31 11:56:32 +0200

    Use partitioned spatial join

--- src/main/scala/com/pg/spark/SelfIntersectionsCalculator.scala
+++ src/main/scala/com/pg/spark/SelfIntersectionsCalculator.scala
@@ -3,8 +3,10 @@ package com.pg.spark
 import model.Polygon
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import spatialspark.join.BroadcastSpatialJoin
+import spatialspark.join.PartitionedSpatialJoin
 import spatialspark.operator.SpatialOperator.Intersects
+import spatialspark.partition.fgp.FixedGridPartitionConf
+import spatialspark.util.MBR
 
 object SelfIntersectionsCalculator {
 
@@ -13,14 +15,24 @@ object SelfIntersectionsCalculator {
     */
   def calculate(sc: SparkContext, polygons: RDD[Polygon]): RDD[(Polygon, Polygon)] = {
     val geometryById = polygons.filter(!_.geometry.isEmpty).map(p => (p.id, p.geometry))
+    if (geometryById.count() == 0) {
+      return sc.emptyRDD
+    }
     geometryById.cache()
 
-    val intersectedIds: RDD[(Long, Long)] = BroadcastSpatialJoin(sc, geometryById, geometryById, Intersects, 0.0)
+    val extent = polygons.map(_.geometry.getEnvelopeInternal)
+      .map(x => (x.getMinX, x.getMinY, x.getMaxX, x.getMaxY))
+      .reduce((a, b) => (a._1 min b._1, a._2 min b._2, a._3 max b._3, a._4 max b._4))
+
+    val conf = new FixedGridPartitionConf(32, 32, new MBR(extent._1, extent._2, extent._3, extent._4))
+
+    val intersectedIds: RDD[(Long, Long)] =
+      PartitionedSpatialJoin(sc, geometryById, geometryById, Intersects, 0.0, conf)
 
     // join with source data
-    val polgonsById = polygons.map(p => (p.id, p))
-    polgonsById.cache()
-    intersectedIds.join(polgonsById).map(_._2).join(polgonsById).map(_._2)
+    val polygonsById = polygons.map(p => (p.id, p))
+    polygonsById.cache()
+    intersectedIds.join(polygonsById).map(_._2).join(polygonsById).map(_._2)
   }
 
 }
